3abd40fffafef24b91285e902b00cd3b2b0e3d8e
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-11-30 16:16:57 -0500

    fully functional

---
+++ public/javascripts/messages.js
@@ -0,0 +1,14 @@
+const messages = [
+  {
+    text: "I really am not sure of what to say.",
+    user: "Amando",
+    added: new Date().toLocaleDateString("en-US"),
+  },
+  {
+    text: "I just came here to tell you that this is a really good express site that you've made.  I quite like it.",
+    user: "Charles",
+    added: new Date().toLocaleDateString("en-US"),
+  },
+];
+
+module.exports = messages;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const messages = require("../public/javascripts/messages");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages });
+  next();
+});
+
+// get new message
+router.post("/new", (req, res, next) => {
+  const { user, text } = req.body;
+  messages.unshift({
+    user,
+    text,
+    added: new Date().toLocaleDateString("en-US"),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
